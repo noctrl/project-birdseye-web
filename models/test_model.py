1c49151a8e7d6d172981f55a34189fc664ea923d
Michael Holler <apotheos121@gmail.com>
2012-10-24 18:28:29 -0500

    Added table definition for lots.

--- models/test_model.py
+++ models/test_model.py
@@ -1,6 +1,30 @@
 __author__ = 'mjholler'
 
 from sqlalchemy.ext.declarative import delcarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import Boolean
 
 # Classes and tables relative to Base
-Base = delcarative_base()
+Base = delcarative_base() # Declaring the base
+
+class Lots(Base):
+    __tablename__ = 'lots'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    location = Column(String)
+    total_spaces = Column(Integer)
+    available_spaces = Column(Integer)
+    active = Column(Boolean)
+
+    def __init__(self, name, location, total_spaces, available_spaces, active):
+        self.active = name
+        self.location = location
+        self.available_spaces = available_spaces
+        self.total_spaces = total_spaces
+        self.active = active
+
+
+
+
+
