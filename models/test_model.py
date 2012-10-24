7fb048374cc3db5fc911f310516b6a544fd42c17
Michael Holler <apotheos121@gmail.com>
2012-10-24 18:09:31 -0500

    Added dummy files to maintain folder structure.

---
+++ models/test_model.py
@@ -0,0 +1,6 @@
+__author__ = 'mjholler'
+
+from sqlalchemy.ext.declarative import delcarative_base
+
+# Classes and tables relative to Base
+Base = delcarative_base()
--- routes.py
+++ routes.py
@@ -2,9 +2,14 @@ import webapp2
 
 # Local imports
 from handlers.test import test_handler
+from handlers.rest import rest_handler
 
 __author__ = 'mjholler'
 
 app = webapp2.WSGIApplication([
     ('/', test_handler.HelloBirdseye),
+    ('/api/rest/lots', rest_handler.lots),
+    ('/api/rest/lot/[0-9]+', rest_handler.lot),
+    ('/api/rest/sensors', rest_handler.sensors),
+    ('/api/rest/sensor/[0-9]+', rest_handler.sensor)
 ], debug=True)
