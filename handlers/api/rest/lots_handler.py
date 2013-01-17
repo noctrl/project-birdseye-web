872deaa650758b0b6e1d341d992f442c17c17076
Michael Holler <apotheos121@gmail.com>
2013-01-16 22:47:06 -0600

    MORE SQL!!!

--- handlers/api/rest/lots_handler.py
+++ handlers/api/rest/lots_handler.py
@@ -9,28 +9,23 @@ class Lots(webapp2.RequestHandler):
         sql = """
             SELECT * from birdseye.Lots;
         """
-        print 'hello'
         engine = sqlalchemy.create_engine('mysql+pymysql://birdseye:password@localhost/birdseye')
         #engine = sqlalchemy.create_engine('mysql://birdseye:password@localhost/birdseye')
-        print '2'
         conn = engine.connect()
-        print '3'
         result = conn.execute(sql)
-        print '4'
         conn.close()
-        print '5'
 
         output = ""
         for row in result:
-            print "a row:",row
-        self.response.write(result)
+            output += str(row) + "<br />"
+        self.response.write(str(output))
 
     def post(self):
         sql = """
             INSERT INTO birdseye.Lots (name, total_spaces) VALUES ('abcdefg', 60);
         """
         #engine = sqlalchemy.create_engine('mysql+pymysql://birdseye:password@localhost/birdseye')
-        engine = sqlalchemy.create_engine('mysql://birdseye:password@localhost/birdseye')
+        engine = sqlalchemy.create_engine('mysql+pymysql://birdseye:password@localhost/birdseye')
         conn = engine.connect()
 
         transaction = conn.begin()
